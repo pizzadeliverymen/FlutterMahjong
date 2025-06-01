c46c2e73ffcd49a5954553bde5c4e4fdc1c29349
Peder S <schroederpeder@gmail.com>
2025-05-31 20:18:58 -0400

    created basic Meld class

---
+++ bin/melds.dart
@@ -0,0 +1,50 @@
+import 'package:mahjong/utils.dart';
+
+class Meld {
+    
+  MeldType type;
+
+  List<TileSet> tiles;
+
+  PlayerDirection direction;
+
+  TileSet stolenTile;
+
+  bool fromPung;
+
+  Meld({
+    required this.type,
+    required this.tiles,
+    required this.stolenTile,
+    required this.direction,
+    this.fromPung = false
+  });
+
+  getHandTiles() {
+    return tiles;
+  }
+
+  getStolenTile() {
+    return stolenTile;
+  }
+
+  getDirection() {
+    return direction;
+  }
+
+  getType() {
+    return type;
+  }
+
+  isKongFromPung() {
+    return fromPung;
+  }
+
+  @override
+  String toString() {
+    return "$type of ${tiles.join(", ")} : $stolenTile";;
+  }
+}
+
+
+
